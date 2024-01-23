3f3715a8e4ae84886fc693cb3d12dff4bd5c926b
FightClubHaakz <155555109+FightClubHaakz@users.noreply.github.com>
2024-01-23 11:40:05 +0100

    Add files via upload

---
+++ portfolio/gitaristproject/Gitarister/index.js
@@ -0,0 +1,19 @@
+const LMBtn = document.getElementById("lightMode");
+LMBtn.addEventListener("click", toggleLightMode);
+
+let isLightMode = false;
+
+function toggleLightMode() {
+    isLightMode = !isLightMode; // Toggle the state
+
+    if (isLightMode === true) {
+        document.body.style.filter = "invert(95%) hue-rotate(180deg)";
+        document.getElementById("introBox").style.filter = "invert(95%) hue-rotate(180deg)";
+        document.getElementById("slash").style.filter = "invert(95%) hue-rotate(180deg)";
+        LMBtn.textContent = "Dark Mode";
+    } else {
+        document.body.style.filter = "none"; // Reset the filter
+        document.getElementById("introBox").style.filter = "none"; // Reset the filter for the specific element
+        LMBtn.textContent = "Light Mode";
+    }
+}
