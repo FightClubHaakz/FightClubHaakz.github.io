87ab6c6cc5800c6401933754c4dd7715449a98fd
FightClubHaakz <155555109+FightClubHaakz@users.noreply.github.com>
2024-01-24 13:35:04 +0100

    Update index.js

--- portfolio/photosOfMe/index.js
+++ portfolio/photosOfMe/index.js
@@ -1,18 +1,22 @@
-const LMBtn = document.getElementById("lightMode");
-LMBtn.addEventListener("click", toggleLightMode);
+// portfolio.js
+
+const lightModeBtn = document.getElementById("lightMode");
+lightModeBtn.addEventListener("click", toggleLightMode);
 
 let isLightMode = false;
 
 function toggleLightMode() {
     isLightMode = !isLightMode; // Toggle the state
 
-    if (isLightMode === true) {
+    if (isLightMode) {
+        // Apply light mode styles
         document.body.style.filter = "invert(95%) hue-rotate(180deg)";
-        document.getElementById("bradPitt1").style.filter = "invert(95%) hue-rotate(180deg)";
-        LMBtn.textContent = "Dark Mode";
+        document.getElementById("bradPittImage").style.filter = "invert(95%) hue-rotate(180deg)";
+        lightModeBtn.textContent = "Dark Mode";
     } else {
-        document.body.style.filter = "none"; // Reset the filter
-        document.getElementById("bradPitt1").style.filter = "none"; // Reset the filter for the specific element
-        LMBtn.textContent = "Light Mode";
+        // Reset styles for dark mode
+        document.body.style.filter = "none";
+        document.getElementById("bradPittImage").style.filter = "none";
+        lightModeBtn.textContent = "Light Mode";
     }
 }
