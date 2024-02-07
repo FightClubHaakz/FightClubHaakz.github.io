811496266a64fb0970ec69bcce6dc145fac4eff9
FightClubHaakz <155555109+FightClubHaakz@users.noreply.github.com>
2024-02-07 14:59:42 +0100

    Update index.js

--- portfolio/main site/index.js
+++ portfolio/main site/index.js
@@ -5,6 +5,7 @@ lightModeBtn.addEventListener("click", toggleLightMode);
 
 let isLightMode = false;
 
+// Function to toggle light mode
 function toggleLightMode() {
     isLightMode = !isLightMode; // Toggle the state
 
@@ -13,10 +14,19 @@ function toggleLightMode() {
         document.body.style.filter = "invert(95%) hue-rotate(180deg)";
         document.getElementById("introBox").style.filter = "invert(95%) hue-rotate(180deg)";
         lightModeBtn.textContent = "Dark Mode";
+        sessionStorage.lightModeEnable = true;
     } else {
         // Reset styles for dark mode
         document.body.style.filter = "none";
         document.getElementById("introBox").style.filter = "none";
         lightModeBtn.textContent = "Light Mode";
+        sessionStorage.lightModeEnable = false;
     }
+}
+
+// Check if light mode is enabled in session storage
+if (sessionStorage.lightModeEnable === "true") {
+    toggleLightMode(); // Enable light mode if it was previously enabled
+} else {
+    sessionStorage.lightModeEnable = false; // Set it to false if not previously set
 }
